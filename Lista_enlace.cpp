caeb9187e29a552982f0340becebb9ce9dc2e2e0
Jorge Salazar <jorgesadi17@gmail.com>
2026-04-13 13:53:12 -0600

    Lista enlazada con clases

---
+++ Lista_enlace.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+struct Nodo {
+    int dato;
+    Nodo *siguiente;
+};
+
+class Lista {
+    private:
+        Nodo *primero;
+    public:
+    Lista() {
+        primero = NULL;
+    }
+    bool estaVacia() {
+        return primero == NULL;
+    }
+
+    void insertarInicio(int dato){
+        Nodo *nuevo = new Nodo();
+        nuevo->dato = dato;
+        nuevo->siguiente = primero;
+    }
+    void insertarFinal(int dato) {
+        Nodo *nuevo = new Nodo();
+        nuevo->dato = dato;
+        nuevo->siguiente = NULL;
+        nuevo = NULL;
+
+        if (estaVacia()) {
+            primero = nuevo;
+            return;
+        }
+
+        Nodo *actual = primero;
+        while (actual->siguiente != NULL){
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nuevo;
+    }
+};
+
+void menu() {
+    cout << "Lista enlazada" << endl;
+    cout << "***********************************" <<endl;
+    cout << "1. Insertar al inicio" << endl;
+    cout << "2. Insertar al final" << endl;
+    cout << "3. Insertar antes de..." << endl;
+    cout << "4. Insertar despues de..." << endl;
+    cout << "5. Remover primer valor" << endl;
+    cout << "6. Remover ultimo valor" << endl;
+    cout << "7. Remover valor especifico" << endl;
+    cout << "8. Buscar" << endl;
+    cout << "9. Desplegar lista" << endl;
+    cout << "0. Salir" << endl;
+    cout << "Selecciona una opcion: ";
+    cout << endl;
+}
+
+int main() {
+    Lista lista;
+
+    int opcion, valor;
+
+    do {
+       menu();
+        cin >> opcion;
+
+        if (opcion == 1) {
+            cout << "Ingrese un valor: ";
+            cin >> valor;
+            lista.insertarInicio(valor);
+        } else if (opcion == 2) {
+
+        } else if (opcion == 3) {
+
+        } else if (opcion == 4) {
+
+        } else if (opcion == 5) {
+
+        } else if (opcion == 6) {
+
+        } else if (opcion == 7) {
+
+        } else if (opcion == 8) {
+
+        } else if (opcion == 9) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }while (opcion != 0);
+
+    return 0;
+}
