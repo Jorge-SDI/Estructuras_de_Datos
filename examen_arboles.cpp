4e01b011bb0e03ae27125283e7eaa77c7ee4dc44
Jorge Salazar <jorgesadi17@gmail.com>
2026-06-04 23:10:27 -0600

    ejercicios de practica

--- examen_arboles.cpp
+++ examen_arboles.cpp
@@ -81,15 +81,17 @@ Nodo* eliminarValor(Nodo *raiz, int valor) {
 	return raiz;
 }
 
-void invertirArboll(Nodo* raiz){
-	if(raiz == nullptr) return;
+Nodo* invertirArboll(Nodo* raiz){
+	if(raiz == nullptr) return raiz;
 	
 	Nodo* temp = raiz->izquierda;
 	raiz->izquierda = raiz->derecha;
 	raiz->derecha = temp;
 	
-	invertirArboll(raiz->izquierda);
-	invertirArboll(raiz->derecha);
+	raiz->izquierda = invertirArboll(raiz->izquierda);
+	raiz->derecha = invertirArboll(raiz->derecha);
+	
+	return raiz;
 }
 
 Nodo* eliminarHojas(Nodo* raiz){
@@ -156,7 +158,88 @@ void mostrarNivel(Nodo* raiz, int nivelActual, int nivelBuscado){
 	mostrarNivel(raiz->izquierda, nivelActual+1, nivelBuscado);
 	mostrarNivel(raiz->derecha, nivelActual+1, nivelBuscado);
 }
+	
+void mostrarNodosconUnHijo(Nodo* raiz){
+	if(raiz == nullptr){
+		return;
+	}
+	
+	if(raiz->izquierda == nullptr && raiz->derecha != nullptr){
+		cout << raiz->dato << " ";
+	}else if(raiz->izquierda != nullptr && raiz->derecha == nullptr){
+		cout << raiz->dato << " ";
+	}
+	
+	mostrarNodosconUnHijo(raiz->izquierda);
+	mostrarNodosconUnHijo(raiz->derecha);
+}
 
+int mostrarValorMayor(Nodo* raiz){
+	if(raiz == nullptr) return -1;
+	
+	if(raiz->derecha != nullptr){
+		return mostrarValorMayor(raiz->derecha);
+	}
+	
+	return raiz->dato;
+}
+	
+int mostrarAltura(Nodo* raiz){
+	if(raiz == nullptr) return 0;
+	
+	int izquierdaR = mostrarAltura(raiz->izquierda);
+	int derechaR = mostrarAltura(raiz->derecha);
+	
+	if(izquierdaR > derechaR){
+		return 1 + izquierdaR;
+	}
+	
+	return 1 + derechaR;
+	
+}
+	
+bool estaBalanceado(Nodo* raiz){
+	if(raiz == nullptr) return true;
+	
+	bool izqBal = estaBalanceado(raiz->izquierda);
+	bool derBal = estaBalanceado(raiz->derecha);
+	int rIzq = mostrarAltura(raiz->izquierda);
+	int rDer = mostrarAltura(raiz->derecha);
+	int res = abs(rIzq - rDer);
+	
+	if(res <= 1 && izqBal && derBal){
+		return true;
+	}
+	return false;
+}
+	
+int nodosConDosHijos(Nodo* raiz){
+	if(raiz == nullptr) return 0;
+	
+	int contador = 0;
+	if(raiz->izquierda != nullptr && raiz->derecha != nullptr){
+		contador = 1;
+	}
+	
+	int izquierd = nodosConDosHijos(raiz->izquierda);
+	int derech = nodosConDosHijos(raiz->derecha);
+	
+	return contador + izquierd + derech;
+}
+	
+Nodo* eliminarMenoresA(Nodo* raiz, int referencia){
+	if(raiz == nullptr) return raiz;
+	
+	raiz->izquierda = eliminarMenoresA(raiz->izquierda, referencia);
+	raiz->derecha = eliminarMenoresA(raiz->derecha, referencia);
+	
+	if(raiz->dato < referencia){
+		Nodo* temp = eliminarValor(raiz, raiz->dato);
+		return temp;
+	}
+	
+	return raiz;
+}
 void mostrarMenu(){
 	cout << endl;
 	cout << "1. Ingresar valores" << endl;
@@ -166,6 +249,13 @@ void mostrarMenu(){
 	cout << "5. Eliminar valores impares" << endl;
 	cout << "6. Duplicar nodos hojas" << endl;
 	cout << "7. Mostrar nodos de un nivel" << endl;
+	cout << "8. Mostrar nodos coon un solo hijo" << endl;
+	cout << "9. Invertir el arbol" << endl;
+	cout << "10. Valor mas grande del arbol" << endl;
+	cout << "11. Mostrar la altura del arbol" << endl;
+	cout << "12. ¿Está balanceado el arbol?" << endl;
+	cout << "13. Nodos con dos hijos" << endl;
+	cout << "14. Eliminar nodos menores a..." << endl;
 	cout << "Ingrese una opcion: ";
 }
 int main() {
@@ -211,6 +301,42 @@ int main() {
 			cout << endl;
 			cout << "Nodos del nivel " << nivelBuscado << endl;
 			mostrarNivel(raiz, 0, nivelBuscado);
+		}else if(opcion == 8){
+			cout << endl;
+			cout << "Nodos con un solo hijo" << endl;
+			mostrarNodosconUnHijo(raiz);
+		}else if(opcion == 9){
+			cout << endl;
+			cout << "Arbol invertido correctamente" << endl;
+			raiz = invertirArboll(raiz);
+		}else if(opcion == 10){
+			cout << endl;
+			int mayor = mostrarValorMayor(raiz);
+			cout << "Valor mas grande del arbol es: " << mayor << endl;
+			cout << endl;
+		}else if(opcion == 11){
+			cout << endl;
+			int alt = mostrarAltura(raiz);
+			cout << "La altura del arbol es: " << alt << endl;
+		}else if(opcion == 12){
+			cout << endl;
+			bool balanceado = estaBalanceado(raiz);
+			if(balanceado != false)
+				cout << "El arbol si esta balanceado" << endl;
+			else
+				cout << "El arbol no esta balanceado" << endl;
+		}else if(opcion == 13){
+			cout << endl;
+			int dosHijos = nodosConDosHijos(raiz);
+			cout << "Hay " << dosHijos << " nodos con dos hijos en el arbol" << endl;
+		}else if(opcion == 14){
+			cout << endl;
+			int val;
+			cout << "Valor de referencia para eliminar";
+			cin >> val;
+			
+			raiz = eliminarMenoresA(raiz, val);
+			cout << "Valores eliminados correctamente" << endl;
 		}else if(opcion == 0){
 			cout << "Saliendo..." << endl;
 		}
